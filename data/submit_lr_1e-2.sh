1ddfbc4475969c46ca5734460739491b79a7b6f8
sofuncheung <sofuncheung@qq.com>
2022-04-11 19:50:36 +0100

    restore submit to original

--- data/submit_lr_1e-2.sh
+++ data/submit_lr_1e-2.sh
@@ -1,12 +1,14 @@
-for dataset in {"FashionMNIST_binary","KMNIST_binary","EMNIST_binary"}
-do
-    for train_size in {100,500,1000,2000,4000,8000,10000,16000}
+for model_depth in 2
+do    
+    for dataset in {"FashionMNIST_binary","KMNIST_binary","EMNIST_binary"}
     do
-        for seed in {0,16,32,64,128,256,512,1024}
+        for train_size in {100,500,1000,2000,4000,8000,10000,16000}
         do
-        addqueue -q gpushort -n 1x4 -m 7 -s /mnt/zfsusers/sofuncheung/.venv/pyhessian/bin/python -m generation.train --dataset_type $dataset --train_dataset_size $train_size --seed $seed
+            for seed in {0,16,32,64,128,256,512,1024}
+            do
+            addqueue -q gpushort -n 1x4 -m 7 -s /mnt/zfsusers/sofuncheung/.venv/pyhessian/bin/python -m generation.train --model_depth $model_depth --dataset_type $dataset --train_dataset_size $train_size --seed $seed
+            done
         done
     done
 done
-
 
